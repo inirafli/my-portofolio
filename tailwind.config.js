e8be2c168e93f491123cfa94342eb0242d9ad56a
inirafli <raplirayhann@gmail.com>
2024-01-12 14:30:54 +0700

    added hero element

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
         primary: '#fcd34d',
